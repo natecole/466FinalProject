be9a3fdb556d3ca26a9965822b87e0284d848cbc
asjshiva <asj.shiva@gmail.com>
2023-03-22 18:50:08 -0700

    Add evaluation

--- src/Demo.java
+++ src/Demo.java
@@ -15,6 +15,7 @@ public class Demo {
     public static double[][] trainingData, testingData;
     public static int[] trainingTargets, testingTargets;
     public static int trainTestSplit;
+    public static double[] results;
 
     public static void processData(String filename) {
         try (CSVReader reader = new CSVReader(new FileReader(PATH+filename))) {
@@ -54,8 +55,68 @@ public class Demo {
 
         p.train(trainingData, trainingTargets, 100);
 
-        //TODO: test model
+        results = new double[testingTargets.length];
+        for(int i = 0; i < testingData.length; i++){
+            results[i] = p.predict(testingData[i]) >= 0 ? 0 : 1;
+        }
 
-        
+        evaluateClusters();
+    }
+
+    public static void evaluateClusters() {
+        // 2 clusters
+
+        // Cluster 1, which contains indices where result = 1
+        int TPCluster1 = 0;
+        int FPCluster1 = 0;
+        int TNCluster1 = 0;
+        int FNCluster1 = 0;
+
+        // Cluster 0, which contains indices where result = 0
+        int TPCluster0 = 0;
+        int FPCluster0 = 0;
+        int TNCluster0 = 0;
+        int FNCluster0 = 0;
+
+        for(int i = 0; i < testingTargets.length; i++){
+            if(results[i] == testingTargets[i]){
+                // result = 1, Actual = 1
+                if(results[i] == 1) {
+                    TPCluster1 += 1;
+                    FPCluster0 += 1;
+                }
+                // result = 0, Actual = 0
+                else{
+                    TPCluster0 += 1;
+                    FPCluster1 += 1;
+                }
+            }
+            else if(results[i] != testingTargets[i]){
+                // result = 1, Actual = 0
+                if(results[i] == 1){
+                    FPCluster1 += 1;
+                    FNCluster0 += 1;
+                }
+                // result = 0, Actual = 1
+                else{
+                    FPCluster0 += 1;
+                    FNCluster1 += 1;
+                }
+            }
+        }
+
+        System.out.println("Cluster 1");
+        System.out.printf("True Positive : %d\n" +
+                "True Negative : %d\n" +
+                "False Positive : %d\n" +
+                "False Negative : %d\n\n",
+                TPCluster1, TNCluster1, FPCluster1, FNCluster1);
+
+        System.out.println("Cluster 0");
+        System.out.printf("True Positive : %d\n" +
+                "True Negative : %d\n" +
+                "False Positive : %d\n" +
+                "False Negative : %d\n\n",
+                TPCluster0, TNCluster0, FPCluster0, FNCluster0);
     }
 }
