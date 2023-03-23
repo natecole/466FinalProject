9e032a82937ab300b0d918bc17e27e11c2ebf77b
natecole <natelcole88@gmail.com>
2023-03-22 17:22:03 -0700

    fixing csv reader

--- src/Demo.java
+++ src/Demo.java
@@ -1,30 +1,35 @@
-package com.mkyong.io.csv.opencsv;
-
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.io.IOException;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
 
+import DocumentClasses.*;
 import NeuralNetwork.*;
-import project.FinalProject466.DocumentClasses.*;
+import com.opencsv.exceptions.CsvValidationException;
 
 public class Demo {
-    public static final String PATH = "";
+    public static final String PATH = "./files/";
     public static ArrayList<Wine> data = new ArrayList<Wine>();
     public static double[] targets;
 
     public static void processData(String filename) {
         try (CSVReader reader = new CSVReader(new FileReader(PATH+filename))) {
             String[] lineInArray;
-            while ((lineInArray = reader.nextLine().replaceAll
-            ("\\s", "").split(";")) != null) {
+            while ((lineInArray = reader.readNext()) != null) {
+                lineInArray = lineInArray[0].replaceAll("\\s", "").split(";");
                 data.add(new Wine(lineInArray));
             }
+        } catch (CsvValidationException | IOException e) {
+            e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
         processData("winequality-white.csv");
+        for(Wine entry : data){
+            System.out.println(entry.toString());
+        }
     }
 }
--- src/DocumentClasses/Wine.java
+++ src/DocumentClasses/Wine.java
@@ -1,4 +1,4 @@
-package project.FinalProject466.DocumentClasses;
+package DocumentClasses;
 
 public class Wine {
     public double fixed_acidity = 0.0;
@@ -6,8 +6,8 @@ public class Wine {
     public double citric_acid = 0.0;
     public double residual_sugar = 0.0;
     public double chlorides = 0.0;
-    public int free_sulfur_dioxide = 0;
-    public int total_sulfur_dioxide = 0;
+    public double free_sulfur_dioxide = 0;
+    public double total_sulfur_dioxide = 0;
     public double density = 0.0;
     public double pH = 0.0;
     public double sulphates = 0.0;
@@ -38,8 +38,8 @@ public class Wine {
         this.citric_acid = Double.parseDouble(values[2]);
         this.residual_sugar = Double.parseDouble(values[3]);
         this.chlorides = Double.parseDouble(values[4]);
-        this.free_sulfur_dioxide = Integer.parseInt(values[5]);
-        this.total_sulfur_dioxide = Integer.parseInt(values[6]);
+        this.free_sulfur_dioxide = Double.parseDouble(values[5]);
+        this.total_sulfur_dioxide = Double.parseDouble(values[6]);
         this.density = Double.parseDouble(values[7]);
         this.pH = Double.parseDouble(values[8]);
         this.sulphates = Double.parseDouble(values[9]);
@@ -64,8 +64,8 @@ public class Wine {
         res += ", citric_acid: " + Double.toString(citric_acid);
         res += ", residual_sugar: " + Double.toString(residual_sugar);
         res += ", chlorides: " + Double.toString(chlorides);
-        res += ", free_sulfur_dioxide: " + Integer.toString(free_sulfur_dioxide);
-        res += ", total_sulfur_dioxide: " + Integer.toString(total_sulfur_dioxide);
+        res += ", free_sulfur_dioxide: " + Double.toString(free_sulfur_dioxide);
+        res += ", total_sulfur_dioxide: " + Double.toString(total_sulfur_dioxide);
         res += ", density: " + Double.toString(density);
         res += ", pH: " + Double.toString(pH);
         res += ", sulphates: " + Double.toString(sulphates);
